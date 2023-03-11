0066815852b34fe193267d1a306ee6ea7095b854
Soumik Das <79033574+FalconEthics@users.noreply.github.com>
2023-03-11 20:44:14 +0530

    list update

--- Setup.java
+++ Setup.java
@@ -10,45 +10,45 @@ class Setup {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
 
-        //select OS
+        // select OS
         System.out.println(
                 "\n Enter your OS family: \n \t \t \t 1. Linux \n \t \t \t 2. Unix \n \t \t \t 3. Windows \n \t \t \t 4. MacOS \n \t \t \t 5. Others");
         String OS = myObj.nextLine();
 
-        //start setup with selected OS
+        // start setup with selected OS
         if (OS.equalsIgnoreCase("Linux")) {
 
-            //if OS is Linux
+            // if OS is Linux
             System.out.println("Welcome to Linux Programming Environment Setup!");
-            //passing linux.txt to start method
+            // passing linux.txt to start method
             start("linux.txt");
 
         } else if (OS.equalsIgnoreCase("unix") || OS.equalsIgnoreCase("MacOS")) {
 
-            //if OS is Unix or MacOS
+            // if OS is Unix or MacOS
             System.out.println("Welcome to Unix Programming Environment Setup!");
-            //passing unix.txt to start method
+            // passing unix.txt to start method
             start("unix.txt");
 
         } else {
 
-            //if OS is not supported
+            // if OS is not supported
             System.out.println("OS not supported!");
         }
 
     }
 
-    //start method
+    // start method
     public static void start(String option) {
 
         String fileName = option; // Change this to the name of your file
         ArrayList<String> linuxItems = new ArrayList<>();
 
-        //reading file
+        // reading file
         try (
                 BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-            //reading line by line
+            // reading line by line
             while ((line = br.readLine()) != null) {
                 linuxItems.add(line);
             }
@@ -59,23 +59,23 @@ class Setup {
         Scanner myObj = new Scanner(System.in);
         int index = 0;
 
-        //looping through the list
+        // looping through the list
         for (index = 0; index < linuxItems.size(); index++) {
 
-            //printing the list
+            // printing the list
             System.out.println("\n" + linuxItems.get(index));
-            //asking user to complete the step
+            // asking user to complete the step
             System.out.println("\nDone? (y/n)");
             String answer = myObj.nextLine();
 
             if (answer.equalsIgnoreCase("n")) {
-                //if user is not done with the step
+                // if user is not done with the step
                 System.out.println("\nPlease complete the following step first!");
-                //decrementing index to print the same step again
+                // decrementing index to print the same step again
                 index--;
             }
         }
-        //printing message when all steps are completed
+        // printing message when all steps are completed
         System.out.println("\nCongrats your coding environment is ready!");
     }
 }
